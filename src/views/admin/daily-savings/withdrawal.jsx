d5f48c85d096716f2bc99f510a2564522f2f91a9
ojimcy <ojimcy247@gmail.com>
2023-08-27 00:58:54 +0100

    update withdrawal

--- src/views/admin/daily-savings/withdrawal.jsx
+++ src/views/admin/daily-savings/withdrawal.jsx
@@ -16,10 +16,10 @@ import axiosService from 'utils/axiosService';
 import BackButton from 'components/menu/BackButton';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
-import { useHistory } from 'react-router-dom/';
-import { useAppContext } from 'contexts/AppContext';
+import { useHistory, useParams } from 'react-router-dom/';
 
 export default function Withdrawal() {
+  const { packageId } = useParams();
   const history = useHistory();
   const brandStars = useColorModeValue('brand.500', 'brand.400');
   const textColor = useColorModeValue('navy.700', 'white');
@@ -28,22 +28,17 @@ export default function Withdrawal() {
     handleSubmit,
     register,
     formState: { errors, isSubmitting },
-    setValue,
   } = useForm();
 
-  const [user, setUser] = useState({});
-  const [accountNumber, setAccountNumber] = useState(null);
-
-  const { customerData, setPackageId } = useAppContext();
+  const [userPackage, setUserPackage] = useState({});
 
   useEffect(() => {
     const fetchUserPackage = async () => {
       try {
-        const packages = await axiosService.get(
-          `/daily-savings/package?userId=${customerData.userId}&accountNumber=${customerData.accountNumber}`
+        const response = await axiosService.get(
+          `/daily-savings/package/${packageId}`
         );
-        setPackageId(packages.data.id);
-        setAccountNumber(customerData.accountNumber);
+        setUserPackage(response.data);
       } catch (error) {
         console.error(error);
         toast.error(
@@ -54,46 +49,27 @@ export default function Withdrawal() {
     };
 
     fetchUserPackage();
-    // Set the initial value for "Account Number" using setValue
-    if (customerData.accountNumber) {
-      setValue('accountNumber', customerData.accountNumber);
-    }
-  }, [customerData, setPackageId, setValue]);
-
-  const fetchUserByAccountNumber = useCallback(async (accountNumber) => {
-    try {
-      const response = await axiosService.get(
-        `/transactions/user/?accountNumber=${accountNumber}`
-      );
-      setUser(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (accountNumber) {
-      fetchUserByAccountNumber(accountNumber);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [accountNumber]);
+  }, [packageId]);
 
   // Handle form submission
   const handleContributionSubmit = useCallback(
     async (withdrawData) => {
       try {
-        await axiosService.post(`/daily-savings/withdraw`, withdrawData);
-        toast.success('Withdraw successful!');
-        history.push(`/admin/customer/${customerData.userId}`);
+        await axiosService.post(
+          `/daily-savings/withdraw`,
+          withdrawData
+        );
+        toast.success('Contribution successful!');
+        history.push(`/admin/default`);
       } catch (error) {
         console.error(error);
         toast.error(
           error.response?.data?.message ||
-            'An error occurred while fetching user package.'
+            'An error occurred while making contribution.'
         );
       }
     },
-    [history, customerData.userId]
+    [history]
   );
 
   return (
@@ -141,20 +117,15 @@ export default function Withdrawal() {
                     mb="24px"
                     fontWeight="500"
                     size="lg"
-                    defaultValue={accountNumber}
+                    defaultValue={userPackage.accountNumber || ''}
                     {...register('accountNumber', {
                       required: 'Account number is required',
                     })}
-                    onBlur={(e) => {
-                      const accountNumber = e.target.value.trim();
-                      if (accountNumber) {
-                        fetchUserByAccountNumber(accountNumber);
-                      }
-                    }}
                   />
-                  {user ? (
+                  {userPackage && userPackage ? (
                     <Text fontSize="sm" color="green" mt="2px" pb="10px">
-                      {user.firstName} {user.lastName}
+                      {userPackage.userId?.firstName}{' '}
+                      {userPackage.userId?.lastName}
                     </Text>
                   ) : (
                     <Text fontSize="sm" color="red" mt="2px" pb="10px">
@@ -163,6 +134,37 @@ export default function Withdrawal() {
                   )}
                   <FormErrorMessage>
                     {errors.accountNumber && errors.accountNumber.message}
+                  </FormErrorMessage>
+                </FormControl>
+                <FormControl isInvalid={errors.target}>
+                  <FormLabel
+                    htmlFor="target"
+                    display="flex"
+                    ms="4px"
+                    fontSize="sm"
+                    fontWeight="500"
+                    color={textColor}
+                    mb="8px"
+                  >
+                    Target<Text color={brandStars}>*</Text>
+                  </FormLabel>
+                  <Input
+                    isRequired={true}
+                    variant="auth"
+                    fontSize="sm"
+                    ms={{ base: '0px', md: '0px' }}
+                    type="text"
+                    id="target"
+                    mb="24px"
+                    fontWeight="500"
+                    size="lg"
+                    defaultValue={userPackage.target || ''}
+                    {...register('target', {
+                      required: 'Target is required',
+                    })}
+                  />
+                  <FormErrorMessage>
+                    {errors.target && errors.target.message}
                   </FormErrorMessage>
                 </FormControl>
                 <FormControl isInvalid={errors.amount}>
@@ -198,7 +200,7 @@ export default function Withdrawal() {
 
                 <Box width={{ base: '50%', md: '50%', sm: '50%' }} mt="15px">
                   <Button
-                    colorScheme="green"
+                    colorScheme="red"
                     variant="solid"
                     w="100%"
                     h="50"
