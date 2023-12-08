3667432d97385eadf7272267273336a64b519fc3
ojimcy <ojimcy247@gmail.com>
2023-12-09 00:57:37 +0100

    updat

--- src/utils/axiosService.js
+++ src/utils/axiosService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3001/v1';
-// const baseURL = 'https://l1uwu6hw65.execute-api.us-east-1.amazonaws.com/v1';
+// const baseURL = 'http://localhost:3001/v1';
+const baseURL = 'https://l1uwu6hw65.execute-api.us-east-1.amazonaws.com/v1';
 
 const api = axios.create({
   baseURL,
