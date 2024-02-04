fac5a01f73d7f364b9c8bf2e0c8b73d5bb6952fb
Emmanuel <ojimcy247@gmail.com>
2024-02-04 06:13:23 +0100

    filter transactions

--- src/components/transactions/RecentTransactions.js
+++ src/components/transactions/RecentTransactions.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
   Box,
   Flex,
@@ -15,6 +15,8 @@ import axiosService from 'utils/axiosService';
 import { useAppContext } from 'contexts/AppContext';
 import TransactionItem from 'components/transactions/TransactionItem';
 import { useAuth } from 'contexts/AuthContext';
+import { formatDate } from 'utils/helper';
+import CustomDateModal from 'components/modals/CustomDateModal';
 
 function RecentTransactions() {
   const { customerData } = useAppContext();
@@ -22,8 +24,37 @@ function RecentTransactions() {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredTransaction, setFilteredTransaction] = useState([]);
   const [selectedFilter, setSelectedFilter] = useState('all');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
   const [transactions, setTransactions] = useState([]);
   const [showAllTransactions, setShowAllTransactions] = useState(false);
+  const [isCustomDateModalOpen, setCustomDateModalOpen] = useState(false);
+  const [customRangeLabel, setCustomRangeLabel] = useState('Custom Range');
+  const [timeRange, setTimeRange] = useState('all');
+
+  const handleSelectChange = (e) => {
+    const selectedValue = e.target.value;
+
+    if (selectedValue === 'custom') {
+      setCustomDateModalOpen(true);
+    } else {
+      setTimeRange(selectedValue);
+    }
+  };
+
+  const handleStartDateChange = useCallback(
+    (e) => {
+      setStartDate(e.target.value);
+    },
+    [setStartDate]
+  );
+
+  const handleEndDateChange = useCallback(
+    (e) => {
+      setEndDate(e.target.value);
+    },
+    [setEndDate]
+  );
 
   useEffect(() => {
     const fetchActivities = async () => {
@@ -50,8 +81,8 @@ function RecentTransactions() {
         );
       } else if (selectedFilter === 'withdrawal') {
         return (
-          transaction.narration !== 'Daily contribution' &&
-          !transaction.narration !== 'SB Daily contribution'
+          transaction.narration === 'Daily contribution withdrawal' ||
+          transaction.narration === 'Request Cash'
         );
       }
       return false;
@@ -80,6 +111,53 @@ function RecentTransactions() {
 
   const shouldShowViewAllButton = filteredTransaction.length > 20;
 
+  useEffect(() => {
+    let filteredData = transactions;
+
+    if (timeRange === 'last7days') {
+      const last7Days = new Date();
+      last7Days.setDate(last7Days.getDate() - 7);
+      filteredData = filteredData.filter(
+        (item) => new Date(item.date) >= last7Days
+      );
+    } else if (timeRange === 'last30days') {
+      const last30Days = new Date();
+      last30Days.setDate(last30Days.getDate() - 30);
+      filteredData = filteredData.filter(
+        (item) => new Date(item.date) >= last30Days
+      );
+    } else if (timeRange === 'custom') {
+      if (startDate && endDate) {
+        const customStartDate = new Date(startDate);
+        customStartDate.setHours(0, 0, 0, 0);
+        const customEndDate = new Date(endDate);
+        customEndDate.setHours(23, 59, 59, 999);
+        filteredData = filteredData.filter(
+          (item) =>
+            new Date(item.date) >= customStartDate &&
+            new Date(item.date) <= customEndDate
+        );
+      }
+    }
+    setFilteredTransaction(filteredData);
+  }, [timeRange, startDate, endDate, transactions]);
+
+  const handleCustomDateApply = useCallback(
+    (selectedStartDate, selectedEndDate) => {
+      if (selectedStartDate && selectedEndDate) {
+        const formattedStartDate = formatDate(selectedStartDate);
+        const formattedEndDate = formatDate(selectedEndDate);
+
+        setCustomRangeLabel(`${formattedStartDate} to ${formattedEndDate}`);
+        setStartDate(selectedStartDate);
+        setEndDate(selectedEndDate);
+        setTimeRange('custom');
+      }
+      setCustomDateModalOpen(false);
+    },
+    []
+  );
+
   return (
     <Box mt="80px">
       <Flex
@@ -102,6 +180,12 @@ function RecentTransactions() {
               <option value="all">All</option>
               <option value="deposit">Deposit</option>
               <option value="withdrawal">Withdrawal</option>
+            </Select>
+            <Select value={timeRange} onChange={handleSelectChange}>
+              <option value="all">All Time</option>
+              <option value="last7days">Last 7 Days</option>
+              <option value="last30days">Last 30 Days</option>
+              <option value="custom">{customRangeLabel}</option>
             </Select>
             <FormControl>
               <Input
@@ -137,6 +221,16 @@ function RecentTransactions() {
       ) : (
         <Text>Transaction not found</Text>
       )}
+
+      <CustomDateModal
+        isOpen={isCustomDateModalOpen}
+        onClose={() => setCustomDateModalOpen(false)}
+        startDate={new Date(startDate)}
+        endDate={new Date(endDate)}
+        handleStartDateChange={handleStartDateChange}
+        handleEndDateChange={handleEndDateChange}
+        handleCustomDateApply={handleCustomDateApply}
+      />
     </Box>
   );
 }
